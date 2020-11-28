2d372897cdc084204b8a3bdcc866e532857ef30b
computer tech <developer.krishnasoftweb@gmail.com>
2020-11-28 13:19:52 +0530

    add app

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:crunch/LoginScreen/Login.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/",
+      routes: {
+        "/": (context) => Login(),
+      },
+    );
+  }
+}
