14e234e90629ea417ba270f643ab90d4836e8f7b
krishnasoftweb <developer.krishnasoftweb@gmail.com>
2021-01-18 15:14:46 +0530

    added checkout button

--- lib/Screens/cart.dart
+++ lib/Screens/cart.dart
@@ -1,5 +1,6 @@
 import 'package:crunch/APIS/Constants.dart';
 import 'package:crunch/Common/classes.dart';
+import 'package:crunch/Static/Constant.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,7 @@ class Cart extends StatefulWidget {
 
 class _CartState extends State<Cart> {
   List<CartData> cartItems = [];
+  double grandTotal = 0;
   @override
   void initState() {
     getCartData();
@@ -20,6 +22,9 @@ class _CartState extends State<Cart> {
     var cartData = await SQFLiteTables.getData(table: Tables.CART);
     for (int i = 0; i < cartData.length; i++) {
       setState(() {
+        grandTotal += (double.parse(cartData[i]["item_price"]) *
+                double.parse(cartData[i]["qty"])) +
+            double.parse(cartData[i]["combined_price"]);
         cartItems.add(CartData(
             itemPrice: cartData[i]["item_price"].toString(),
             itemName: cartData[i]["item_name"].toString(),
@@ -33,54 +38,83 @@ class _CartState extends State<Cart> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Cart",
-            style: TextStyle(color: Colors.black),
-          ),
-          backgroundColor: Colors.white,
-          elevation: 1,
+      appBar: AppBar(
+        title: Text(
+          "Cart",
+          style: TextStyle(color: Colors.black),
         ),
-        body: cartItems.length > 0
-            ? ListView.builder(
-                itemBuilder: (BuildContext context, int index) {
-                  double total = (double.parse(cartItems[index].itemPrice) *
-                          double.parse(cartItems[index].qty)) +
-                      double.parse(cartItems[index].combinedPrice);
-                  return ExpansionTile(
-                    title: Text(
-                      "${cartItems[index].itemName}",
+        backgroundColor: Colors.white,
+        elevation: 1,
+      ),
+      body: cartItems.length > 0
+          ? ListView.builder(
+              itemBuilder: (BuildContext context, int index) {
+                double total = (double.parse(cartItems[index].itemPrice) *
+                        double.parse(cartItems[index].qty)) +
+                    double.parse(cartItems[index].combinedPrice);
+                return ExpansionTile(
+                  title: Text(
+                    "${cartItems[index].itemName}",
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Text(
+                      "Grand total : $total",
                       style:
                           TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
                     ),
-                    subtitle: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: Text(
-                        "Grand total : $total",
-                        style: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    childrenPadding:
-                        EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    children: [
-                      buildTitledRow(
-                          title: "Price", val: cartItems[index].itemPrice),
-                      buildTitledRow(title: "Qty", val: cartItems[index].qty),
-                      buildTitledRow(
-                          title: "Add on price",
-                          val: cartItems[index].combinedPrice),
-                      Divider(),
-                      buildTitledRow(title: "Total", val: total.toString()),
-                    ],
-                  );
-                },
-                itemCount: cartItems.length,
-              )
-            : Center(
-                child: Text("Your cart is empty."),
-              ));
+                  ),
+                  childrenPadding:
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  children: [
+                    buildTitledRow(
+                        title: "Price", val: cartItems[index].itemPrice),
+                    buildTitledRow(title: "Qty", val: cartItems[index].qty),
+                    buildTitledRow(
+                        title: "Add on price",
+                        val: cartItems[index].combinedPrice),
+                    Divider(),
+                    buildTitledRow(title: "Total", val: total.toString()),
+                  ],
+                );
+              },
+              itemCount: cartItems.length,
+            )
+          : Center(
+              child: Text("Your cart is empty."),
+            ),
+      floatingActionButton: cartItems.length > 0 ? Container(
+        height: 50,
+        width: size.width * 0.9,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6),
+          color: appPrimaryColors[100],
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          children: [
+            Expanded(
+                child: Text(
+              "Grand Total : $grandTotal",
+              style: TextStyle(
+                  color: appPrimaryMaterialColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14),
+            )),
+            Expanded(
+                child: FlatButton(
+              child: Text("CHECKOUT", style: TextStyle(color: Colors.white, fontSize: 14),),
+              color: appPrimaryMaterialColor,
+              onPressed: () {},
+              padding: EdgeInsets.symmetric(vertical: 13),
+            )),
+          ],
+        ),
+      ) : null
+    );
   }
 
   Widget buildTitledRow({String title, String val}) {
