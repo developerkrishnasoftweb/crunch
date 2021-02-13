fbdb626ac1137a90ee753e6c79dcbf5468b45750
developerkrishnasoftweb <developer.krishnasoftweb@gmail.com>
2021-02-13 15:14:28 +0530

    added foreground messaging in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,6 @@ class _MyAppState extends State<MyApp> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     _firebaseMessaging.configure(
         onMessage: (Map<String, dynamic> message) async {
